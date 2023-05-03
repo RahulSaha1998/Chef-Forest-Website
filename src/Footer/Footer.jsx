627c1702c0d40fc0ea9facca180a3432cb1f0ba1
Rahul Saha <rahulsaha87928@gmail.com>
2023-05-03 15:12:26 +0600

    footer customized

--- src/Footer/Footer.jsx
+++ src/Footer/Footer.jsx
@@ -4,7 +4,18 @@ import { Container, Navbar } from 'react-bootstrap';
 const Footer = () => {
     return (
         <Container className='mt-5'>
-            
+            <Navbar bg="dark" variant="dark" className='rounded' style={{ height: '200px' }}>
+                <Container>
+                    <Navbar.Text>
+                        © 2023 Example Company. All rights reserved. 
+                        <li>Address: "123 Main St, Anytown USA"</li>
+                        <li>
+                            Phone number: "(123) 456-7890"
+                        </li>
+                        <li>Email address: "info@chiefrestaurant.com"</li>
+                    </Navbar.Text>
+                </Container>
+            </Navbar>
         </Container>
     );
 };
